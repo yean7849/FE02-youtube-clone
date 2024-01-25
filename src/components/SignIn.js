2da93077a8afcfbecf2e06f3e2b0909b1a70a667
Yellni99 <148676218+Yellni99@users.noreply.github.com>
2024-01-25 14:54:12 +0900

    로그인 성공 및 실패 로직, 로그인 버튼 활성화
    로그인 성공 및 실패 로직과 이메일, 비밀번호가 조건에 맞게 작성됐을 때 로그인 버튼이 활성화될 수 있게 로직을 완성했습니다.

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,21 +1,60 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./SignIn.css";
 
-const SignIn = () => {
+const User = {
+  email: "test@test.com",
+  password: "@test12345",
+};
+
+export default function SignIn() {
   const navigate = useNavigate();
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [emailValid, setEmailValid] = useState(false);
+  const [passwordValid, setPasswordValid] = useState(false);
+  const [notNext, setNotNext] = useState(false);
+
   const [remembeId, setRememberId] = useState(false);
 
   const loginHandler = () => {
-    //로그인 로직 구현
+    if (email === User.email && password === User.password) {
+      alert("로그인에 성공했습니다.");
+    } else {
+      alert("등록되지 않은 회원입니다.");
+    }
   };
 
-  const signupHandler = () => {
-    //회원가입 로직 구현
+  const LoginEmailHandler = (event) => {
+    setEmail(event.target.value);
+    const regex = /[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/g;
+    if (regex.test(email)) {
+      setEmailValid(true);
+    } else {
+      setEmailValid(false);
+    }
   };
+
+  const LoginPasswordHandler = (event) => {
+    setPassword(event.target.value);
+    const regex =
+      /^(?=.*[a-zA-z])(?=.*[0-9])(?=.*[$`~!@$!%*#^?&\\(\\)\-_=+])(?!.*[^a-zA-z0-9$`~!@$!%*#^?&\\(\\)\-_=+]).{8,20}$/;
+    if (regex.test(password)) {
+      setPasswordValid(true);
+    } else {
+      setPasswordValid(false);
+    }
+  };
+
+  useEffect(() => {
+    if (emailValid && passwordValid) {
+      //값을 입력하지 않으면 로그인 버튼 활성화 X
+      setNotNext(false);
+      return;
+    }
+    setNotNext(true);
+  }, [emailValid, passwordValid]);
 
   return (
     <div className="container">
@@ -27,26 +66,42 @@ const SignIn = () => {
             type="text"
             placeholder="이메일"
             value={email}
-            onChange={(event) => setEmail(event.target.value)}
+            onChange={LoginEmailHandler}
           />
         </div>
+        <div className="errorMessageWrap">
+          {!emailValid && email.length > 0 && (
+            <div>올바른 이메일을 입력해주세요.</div>
+          )}
+        </div>
         <br />
-        <input
-          className="login-password"
-          type="password"
-          placeholder="비밀번호"
-          value={password}
-          onChange={(event) => setPassword(event.target.value)}
-        />
+        <div className="from-group">
+          <input
+            className="login-password"
+            type="password"
+            placeholder="비밀번호"
+            value={password}
+            onChange={LoginPasswordHandler}
+          />
+        </div>
+        <div className="errorMessageWrap">
+          {!passwordValid && password.length > 0 && (
+            <div>영문, 숫자, 특수문자 포함 8자 이상 입력하세요.</div>
+          )}
+        </div>
         <div className="remember-id">
           <input
             type="checkbox"
             value={remembeId}
-            onChange={() => setRememberId(!remembeId)}
+            onChange={(event) => setRememberId(event.target.value)}
           />
           아이디저장
         </div>
-        <button className="loginSumbit" onClick={loginHandler} type="button">
+        <button
+          className="loginSumbit"
+          onClick={loginHandler}
+          disabled={notNext}
+        >
           로그인
         </button>
         <br />
@@ -62,6 +117,4 @@ const SignIn = () => {
       </div>
     </div>
   );
-};
-
-export default SignIn;
+}
