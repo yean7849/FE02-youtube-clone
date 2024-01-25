303b76d4bae368599dbbd11f7ff77305d8ecfd19
yean7849 <yean7849@gmail.com>
2024-01-25 19:57:40 +0900

    VideoPage Component complete

--- youtube-clone/src/App.js
+++ youtube-clone/src/App.js
@@ -1,33 +1,10 @@
 import './App.css';
-import Buttons from './video page/Components/Buttons';
-import Channel from './video page/Components/Channel';
-import CommentContents from './video page/Components/CommentContents';
-import Comments from './video page/Components/Comments';
-import Detail from './video page/Components/Detail';
-import RcmVideoList from './video page/Components/RcmVideoList';
-import Video from './video page/Components/Video';
+import VideoPage from './video page/VideoPage';
 
 function App() {
   return (
     <div className='App'>
-      <main>
-        <div className='video-side'>
-          <Video />
-          <div className='video-info'>
-            <div className='title'>동영상 제목</div>
-            <div className='video-info-bar'>
-              <Channel />
-              <Buttons />
-            </div>
-          </div>
-          <Detail />
-          <Comments />
-          <CommentContents />
-        </div>
-        <div className='list-side'>
-          <RcmVideoList />
-        </div>
-      </main>
+      <VideoPage />
     </div>
   );
 }
--- youtube-clone/src/video page/VideoPage.js
+++ youtube-clone/src/video page/VideoPage.js
@@ -1,7 +1,36 @@
 import React from 'react';
+import Buttons from './Components/Buttons';
+import Channel from './Components/Channel';
+import CommentContents from './Components/CommentContents';
+import Comments from './Components/Comments';
+import Detail from './Components/Detail';
+import RcmVideoList from './Components/RcmVideoList';
+import Video from './Components/Video';
+import './VideoPage.css';
 
 const VideoPage = () => {
-  return <div></div>;
+  return (
+    <div>
+      <main>
+        <div className='video-side'>
+          <Video />
+          <div className='video-info'>
+            <div className='title'>동영상 제목</div>
+            <div className='video-info-bar'>
+              <Channel />
+              <Buttons />
+            </div>
+          </div>
+          <Detail />
+          <Comments />
+          <CommentContents />
+        </div>
+        <div className='list-side'>
+          <RcmVideoList />
+        </div>
+      </main>
+    </div>
+  );
 };
 
 export default VideoPage;
