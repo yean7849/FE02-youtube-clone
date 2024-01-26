1ffdee4148e0b04755b393299fc21c7da5613dec
Bhangdongho <bhang97@naver.com>
2024-01-27 05:46:11 +0900

    message

--- youtube-clone/src/App.js
+++ youtube-clone/src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-function App() {
+import Home from "./pages/Home";
+import Today from "./pages/Today";
+import Developer from "./pages/Developer";
+import Webd from "./pages/Webd";
+import Website from "./pages/Website";
+import Gsap from "./pages/Gsap";
+import Port from "./pages/Port";
+import Youtube from "./pages/Youtube";
+import Channel from "./pages/Channel";
+import Video from "./pages/video";
+import Search from "./pages/Search";
+import Not from "./pages/Not";
+import Header from "./components/section/Header";
+import Main from "./components/Main";
+import Footer from "./components/section/Footer";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/today" element={<Today />} />
+          <Route path="/developer" element={<Developer />} />
+          <Route path="/webd" element={<Webd />} />
+          <Route path="/website" element={<Website />} />
+          <Route path="/gsap" element={<Gsap />} />
+          <Route path="/port" element={<Port />} />
+          <Route path="/youtube" element={<Youtube />} />
+          <Route path="/channel/:channelID" element={<Channel />} />
+          <Route path="/video/:videoID" element={<Video />} />
+          <Route path="/search/:searchID" element={<Search />} />
+          <Route path="/*" element={<Not />} />
+        </Routes>
+      </Main>
+      <Footer />
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
---
+++ youtube-clone/src/pages/video.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const video = () => {
+  return <div>Video</div>;
+};
+
+export default video;
