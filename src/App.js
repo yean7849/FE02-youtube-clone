e60d5a2291a3050c0685182fbf55ff4967aef823
yean7849 <yean7849@gmail.com>
2024-01-28 19:16:45 +0900

    cideoId로 접근 가능

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import VideoPage from './video page/VideoPage';
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
 
 const App = () => {
   return (
     <div>
-      <VideoPage />
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path='/video/:videoId'
+            element={<VideoPage></VideoPage>}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
--- src/video page/Components/Channel.js
+++ src/video page/Components/Channel.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Channel = () => {
+const Channel = ({videoId}) => {
   return (
     <div>
       <div className='channel'>
