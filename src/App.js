549fd79310bc682ce2a505a590243056f1f342c3
yean7849 <yean7849@gmail.com>
2024-01-28 17:39:01 +0900

    component UI 완성

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import VideoPage from './video page/VideoPage';
+
+const App = () => {
+  return (
+    <div>
+      <VideoPage />
+    </div>
+  );
+};
+
+export default App;
