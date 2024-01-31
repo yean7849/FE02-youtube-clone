ced74efe16642ad9165ab23be20c64e638b832be
Bhangdongho <144618983+Bhangdongho@users.noreply.github.com>
2024-01-31 23:30:33 +0900

    Add files via upload

---
+++ SignUp.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./main.css";
+import { HiPencilAlt } from "react-icons/hi";
+
+const SignUp = () => {
+  return (
+    <button className="custom-button">
+      <HiPencilAlt />
+    </button>
+  );
+};
+
+export default SignUp;
